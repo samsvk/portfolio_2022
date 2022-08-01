9f10287bc2d1a0cd4c47668d90ad84d5f09b994c
samsvk <samsvk00@gmail.com>
2022-08-01 01:21:51 +0100

    wroking display

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,17 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const item = {
   hidden: (i) => ({
-    y: 5,
-    opacity: 0,
+    y: 20,
+    x: 0,
+    // opacity: 0,
   }),
   visible: (i) => ({
-    opacity: 1,
+    // opacity: 1,
     y: 0,
+    x: 0,
     transition: {
-      ease: "easeInOut",
-      duration: 0.3,
+      ease: [0.16, 0.42, 0.5, 1.06],
+      duration: 0.6,
       delay: i * 0.16,
     },
   }),
@@ -31,57 +33,66 @@ export default function Home() {
           <AnimatePresence>
             {go && (
               <>
-                <motion.span
-                  variants={item}
-                  initial="hidden"
-                  animate="visible"
-                  custom={0}
-                  className="p"
-                >
-                  Samuel graduated in Digital Design and
-                  Development and is now a{" "}
-                </motion.span>
-
-                <motion.span
-                  variants={item}
-                  initial="hidden"
-                  animate="visible"
-                  custom={1}
-                  className="p"
-                >
-                  passionate creative. Self-teaching in modern
-                  web technologies,{" "}
-                </motion.span>
-                <motion.span
-                  variants={item}
-                  initial="hidden"
-                  animate="visible"
-                  custom={2}
-                  className="p"
-                >
-                  Samuel spent the past years building industry
-                  experience in design,{" "}
-                </motion.span>
-                <motion.span
-                  variants={item}
-                  initial="hidden"
-                  animate="visible"
-                  custom={3}
-                  className="p"
-                >
-                  development and strategy. All that empowered by
-                  his passion and
-                </motion.span>
-                <motion.span
-                  variants={item}
-                  initial="hidden"
-                  animate="visible"
-                  custom={4}
-                  className="p"
-                >
-                  ambition drives him to grow brands and products
-                  to it's full potential.
-                </motion.span>
+                <span className="pc">
+                  <motion.span
+                    variants={item}
+                    initial="hidden"
+                    animate="visible"
+                    custom={0}
+                    className="p"
+                  >
+                    Samuel graduated in Digital Design and
+                    Development and is now a{" "}
+                  </motion.span>
+                </span>
+                <span className="pc">
+                  <motion.span
+                    variants={item}
+                    initial="hidden"
+                    animate="visible"
+                    custom={1}
+                    className="p"
+                  >
+                    passionate creative. Self-teaching in modern
+                    web technologies,{" "}
+                  </motion.span>
+                </span>
+                <span className="pc">
+                  <motion.span
+                    variants={item}
+                    initial="hidden"
+                    animate="visible"
+                    custom={2}
+                    className="p"
+                  >
+                    Samuel spent the past years building industry
+                    experience in design,{" "}
+                  </motion.span>
+                </span>
+                <span className="pc">
+                  <motion.span
+                    variants={item}
+                    initial="hidden"
+                    animate="visible"
+                    custom={3}
+                    className="p"
+                  >
+                    development and strategy. All that empowered
+                    by his passion and
+                  </motion.span>
+                </span>
+                <span className="pc">
+                  <motion.span
+                    variants={item}
+                    initial="hidden"
+                    animate="visible"
+                    custom={4}
+                    className="p"
+                  >
+                    ambition drives him to grow brands and
+                    products to it's full potential.
+                  </motion.span>
+                </span>
               </>
             )}
           </AnimatePresence>
