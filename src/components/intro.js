e687a84929e0b961f17ff1609efdfaa529abfb82
samsvk <samsvk00@gmail.com>
2022-08-07 21:11:45 +0100

    animation

--- src/components/intro.js
+++ src/components/intro.js
@@ -38,7 +38,8 @@ const Intro = () => {
   return (
     <div className="flex items-center justify-center max-w-full max-h-full min-h-screen min-w-screen">
       <span className="relative inline-block overflow-hidden text-start text-[70px] text-main-default tracking-tighter leading-[0.9] align-text-top">
-        <div className="flex flex-col whitespace-nowrap h-[145px] bg-main-default w-[120px] justify-start items-start relative">
+        <div className="relative flex flex-col items-start justify-start whitespace-nowrap bg-main-default w-max">
+          <span>0</span>
           <AnimatePresence exitBeforeEnter>
             {active === 0 && (
               <>
@@ -48,7 +49,7 @@ const Intro = () => {
                   animate={"show"}
                   exit={"invisible"}
                   custom={{ duration: 1, delay: 1 }}
-                  className="absolute top-0"
+                  className="absolute top-0 left-0 w-full"
                 >
                   0
                 </motion.span>
@@ -65,7 +66,7 @@ const Intro = () => {
                   animate={"show"}
                   exit={"invisible"}
                   custom={{ duration: 1, delay: 1 }}
-                  className="absolute top-0"
+                  className="absolute top-0 left-0 w-full"
                 >
                   1
                 </motion.span>
@@ -82,15 +83,37 @@ const Intro = () => {
                   animate={"show"}
                   exit={"invisible"}
                   custom={{ duration: 1, delay: 1 }}
-                  className="absolute top-0"
+                  className="absolute top-0 left-0 w-full"
                 >
                   2
                 </motion.span>
               </>
             )}
           </AnimatePresence>
+        </div>
+      </span>
 
-          {/* <AnimatePresence exitBeforeEnter>
+      <span className="relative inline-block overflow-hidden text-start text-[70px] text-main-default tracking-tighter leading-[0.9] align-text-top">
+        <div className="relative flex flex-col items-start justify-start whitespace-nowrap bg-main-default w-max">
+          <span>0</span>
+          <AnimatePresence exitBeforeEnter>
+            {active === 0 && (
+              <>
+                <motion.span
+                  variants={T}
+                  initial={"hidden"}
+                  animate={"show"}
+                  exit={"invisible"}
+                  custom={{ duration: 1, delay: 1 }}
+                  className="absolute top-0 left-0 w-full"
+                >
+                  0
+                </motion.span>
+              </>
+            )}
+          </AnimatePresence>
+
+          <AnimatePresence exitBeforeEnter>
             {active === 1 && (
               <>
                 <motion.span
@@ -98,13 +121,31 @@ const Intro = () => {
                   initial={"hidden"}
                   animate={"show"}
                   exit={"invisible"}
-                  className="absolute bottom-0"
+                  custom={{ duration: 1, delay: 1 }}
+                  className="absolute top-0 left-0 w-full"
                 >
                   1
                 </motion.span>
               </>
             )}
-          </AnimatePresence> */}
+          </AnimatePresence>
+
+          <AnimatePresence exitBeforeEnter>
+            {active === 2 && (
+              <>
+                <motion.span
+                  variants={T}
+                  initial={"hidden"}
+                  animate={"show"}
+                  exit={"invisible"}
+                  custom={{ duration: 1, delay: 1 }}
+                  className="absolute top-0 left-0 w-full"
+                >
+                  2
+                </motion.span>
+              </>
+            )}
+          </AnimatePresence>
         </div>
       </span>
     </div>
