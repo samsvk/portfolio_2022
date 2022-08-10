3a072aa8a357b2da8066bb687937ebf28f3c8f6f
samsvk <samsvk00@gmail.com>
2022-08-10 17:10:58 +0100

    sizeable update

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { PROJECT_DATA } from "../static/constants";
 import { Swipe } from "./swiper";
+import { VscChromeClose } from "react-icons/vsc";
 
 const MODAL = {
   hidden: {
@@ -75,43 +76,70 @@ export const Modal = ({ show, setShow }) => {
             initial="hidden"
             animate="show"
             exit="invisible"
-            className="max-w-[45vw] w-full bg-stone-100 right-0 fixed h-screen z-[100000] "
+            className="max-w-[65vw] w-full bg-stone-100 right-0 fixed h-screen z-[100000] "
           >
             <section>
-              <header className="p-12 text-[26px] md:text-[44px] lg:text-[50px] text-main-default tracking-tighter leading-[0.9] align-text-top font-light">
+              <header className="px-12 pt-12 pb-7 relative text-[26px] md:text-[44px] lg:text-[50px] text-main-default tracking-tighter leading-[0.9] align-text-top font-light">
+                <div
+                  onClick={() => setShow("")}
+                  className="hover:cursor-pointer absolute right-12 h-[33px] w-[33px] bg-white rounded-full drop-shadow-md flex items-center justify-center text-[16px] text-main-secondary "
+                >
+                  <VscChromeClose />
+                </div>
                 <h1>{p.name}</h1>
               </header>
-
-              {<Swipe views={[1, 2, 3, 4, 5]} />}
-
-              <div className="flex p-12 gap-x-10">
-                <div className="w-[60%]">
-                  <h2 className="text-[14px] font-normal tracking-tight text-main-secondary leading-normal align-text-top text-start mb-1">
-                    Overview
-                  </h2>
-                  <p className="tracking-tight text-[14px] align-text-top text-start  list-none text-main-default leading-normal">
-                    {p.overview}
-                  </p>
+              <section className="py-12">
+                <div className="flex w-full flex-col md:flex-row pt-5 gap-3 md:gap-0 md:py-3 font-[400] px-12">
+                  <div className="flex-[0.6] md:flex-1">
+                    <h2 className="text-[14px] font-[400] tracking-tight text-main-secondary leading-normal align-text-top text-start mb-1">
+                      Tech Stack
+                    </h2>
+                    <p className="tracking-tight text-[14px] align-text-top text-start  list-none text-main-default leading-normal">
+                      {p.tech.map((t, i) => (
+                        <span className="mr-1" key={i}>
+                          {t === p.tech[p.tech.length - 1]
+                            ? `${t}.`
+                            : `${t},`}
+                        </span>
+                      ))}
+                    </p>
+                  </div>
+                  <div className="w-[65%]">
+                    <h2 className="text-[14px] font-[400] tracking-tight text-main-secondary leading-normal align-text-top text-start mb-1">
+                      Brief
+                    </h2>
+                    <p className="tracking-tight text-[14px] align-text-top text-start  list-none text-main-default leading-normal 1 max-w-[100%]">
+                      {p.brief}
+                    </p>
+                  </div>
                 </div>
-                <aside className="ml-auto justify-self-end">
-                  <h2 className="text-[14px] font-normal tracking-tight text-main-secondary leading-normal align-text-top text-start mb-1">
-                    Technology
-                  </h2>
-                  <p className="tracking-tight text-[14px] align-text-top text-start  list-none text-main-default leading-normal">
-                    {p.tech.map((t, i) => (
-                      <span className="mr-1" key={i}>
-                        {t}
-                      </span>
-                    ))}
-                  </p>
-                  <h2 className="mt-5 text-[14px] font-normal tracking-tight text-main-secondary leading-normal align-text-top text-start mb-1">
-                    Status
-                  </h2>
-                  <p className="tracking-tight text-[14px] align-text-top text-start  list-none text-main-default leading-normal">
-                    {p.scope[0]}
-                  </p>
-                </aside>
-              </div>
+
+                <div className="flex w-full flex-col md:flex-row pt-12 gap-3 md:gap-0 md:py-3 font-[400] px-12">
+                  <div className="flex-[0.4] md:flex-1">
+                    <h2 className="text-[14px] font-[400] tracking-tight text-main-secondary leading-normal align-text-top text-start mb-1">
+                      Status
+                    </h2>
+                    <p className="tracking-tight text-[14px] align-text-top text-start  list-none text-main-default leading-normal">
+                      {p.scope[0]}
+                    </p>
+                    <h2 className="mt-3 text-[14px] font-[400] tracking-tight text-main-secondary leading-normal align-text-top text-start mb-1">
+                      Site
+                    </h2>
+                    <p className="tracking-tight text-[14px] align-text-top text-start  list-none text-main-default leading-normal relative underline duration-500 max-w-max hover:cursor-pointer hover:opacity-25">
+                      localhost.com
+                    </p>
+                  </div>
+                  <div className="w-[65%]">
+                    <h2 className="text-[14px] font-[400] tracking-tight text-main-secondary leading-normal align-text-top text-start mb-1">
+                      Strategy
+                    </h2>
+                    <p className="tracking-tight text-[14px] align-text-top text-start  list-none text-main-default leading-normal 1 max-w-[100%]">
+                      {p.strategy}
+                    </p>
+                  </div>
+                </div>
+              </section>
+              {<Swipe views={[1, 2, 3, 4, 5]} />}
             </section>
           </motion.div>
         </>
--- src/static/constants.js
+++ src/static/constants.js
@@ -28,30 +28,35 @@ export const PROJECT_DATA = [
   {
     name: "Viperino",
     scope: ["Active", "1 Month"],
-    overview: `Viperino `,
+    brief: `Viperino is an online educational platform providing outstanding courses across various gaming titles. In 2021, Viperino began development with a heavy focus in Valorant. Providing users
+    with unlimited guides through a one time payment model. My role in Viperino was to understand
+    the groups vision for Viperino's visual indetity and bridge that gap into full-scale web app using modern web technologies.
+`,
+    strategy: `I began researching competitors and highly consider their brand strategies. After numerous refrences to other educational sites and internal conversations it became apparent Viperino
+    required a design which emphasised guide content, improved filtering & navigation compared to competitors and most importantly a reliable payment collection method that remained faithful to Viperino's brand identity.`,
     tech: ["Next", "Stripe", "Tailwind"],
   },
-  {
-    name: "Hush",
-    scope: ["Active", "2 Months"],
-    overview:
-      "As a movie fanatic, I found myself spending countless hours trying to find what's next. In a world of digital entertainment, we're spoiled by choice and often we spend more time trying to pick what we want to watch than actually watching. The benefits of Hush become aparent when considering a large group of people trying to decide on what to watch. Hush allows multiple users to vote on what they'd like to watch and present the appropriate selection to the group of users once everyone is ready.",
-    tech: ["Typescript", "SASS", "Node"],
-  },
-  {
-    name: "Mograine",
-    scope: ["Closed", "4 Months"],
-    overview:
-      "Taking into account the RPG values of Warcraft, and Blizzards unique design patterns - the goal was to produce a UI with an art style that Blizzard would ship. When playing Classic WoW, I began to see a recurring theme of the userbase not having the required additional information to make the correct calculations which lead to me building Mograine a now globally used application within the World of Warcraft scene.",
-    tech: ["Typescript", "BEM"],
-  },
-  {
-    name: "Pigfingers",
-    scope: ["Active", "2 Weeks"],
-    overview:
-      "As a typing enthusiast with record breaking scores of over 250WPM+ I had the urge to build a personal typeracer that I had full fledge control over. Pigfingers includes tons of settings, modifiers, gametypes, custom keybinds, and a smooth caret for competitive typists. The typing scene was left grounded when the fan favorite typing services went down with no explaination and that lead me on the journey for building Pigfingers.",
-    tech: ["React", "SASS"],
-  },
+  // {
+  //   name: "Hush",
+  //   scope: ["Active", "2 Months"],
+  //   overview:
+  //     "As a movie fanatic, I found myself spending countless hours trying to find what's next. In a world of digital entertainment, we're spoiled by choice and often we spend more time trying to pick what we want to watch than actually watching. The benefits of Hush become aparent when considering a large group of people trying to decide on what to watch. Hush allows multiple users to vote on what they'd like to watch and present the appropriate selection to the group of users once everyone is ready.",
+  //   tech: ["Typescript", "SASS", "Node"],
+  // },
+  // {
+  //   name: "Mograine",
+  //   scope: ["Closed", "4 Months"],
+  //   overview:
+  //     "Taking into account the RPG values of Warcraft, and Blizzards unique design patterns - the goal was to produce a UI with an art style that Blizzard would ship. When playing Classic WoW, I began to see a recurring theme of the userbase not having the required additional information to make the correct calculations which lead to me building Mograine a now globally used application within the World of Warcraft scene.",
+  //   tech: ["Typescript", "BEM"],
+  // },
+  // {
+  //   name: "Pigfingers",
+  //   scope: ["Active", "2 Weeks"],
+  //   overview:
+  //     "As a typing enthusiast with record breaking scores of over 250WPM+ I had the urge to build a personal typeracer that I had full fledge control over. Pigfingers includes tons of settings, modifiers, gametypes, custom keybinds, and a smooth caret for competitive typists. The typing scene was left grounded when the fan favorite typing services went down with no explaination and that lead me on the journey for building Pigfingers.",
+  //   tech: ["React", "SASS"],
+  // },
 ];
 
 export function getWindow() {
