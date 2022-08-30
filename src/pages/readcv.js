af31a47cf2c08c399097bcda2759549b6990327e
samsvk <samsvk00@gmail.com>
2022-08-31 00:22:23 +0100

    cleanup

--- src/pages/readcv.js
+++ src/pages/readcv.js
@@ -66,11 +66,17 @@ const Resume = () => {
         >
           <div className="relative">
             <div className="rounded-full relative h-[92px] w-[92px] overflow-hidden border border-new-border drop-shadow-md">
-              <Image
+              {/* <Image
                 src="/pic.jpg"
                 layout="fill"
                 objectFit="cover"
                 className="absolute top-[3.8rem!important] bottom-0 scale-[1.7]"
+              /> */}
+              <Image
+                src="/pic.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute top-[3rem!important] bottom-0 scale-[1.7]"
               />
             </div>
           </div>
